23835ab29850f71c4c1e390b9300e754f9d1e9c8
AlBars <103575531+AlBarsStudio@users.noreply.github.com>
2026-04-08 20:31:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/shift-schedule-test/',
+  base: '/park-staff/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
