d38a613c20c129ee02c4e3352b1e8c19dc6e3399
AlBars <103575531+AlBarsStudio@users.noreply.github.com>
2026-04-21 22:59:56 +0300

    Update Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,33 +4,34 @@ import { cn } from '../../utils/cn';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
   icon?: React.ReactNode;
+  fullWidth?: boolean;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, error = false, icon, ...props }, ref) => {
-    const inputClass = error ? 'input-error' : 'input';
-
-    if (icon) {
-      return (
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+  ({ 
+    className, 
+    error = false, 
+    icon,
+    fullWidth = true,
+    ...props 
+  }, ref) => {
+    return (
+      <div className={cn('relative', fullWidth && 'w-full')}>
+        {icon && (
+          <div className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none">
             {icon}
           </div>
-          <input
-            ref={ref}
-            className={cn(inputClass, 'pl-10', className)}
-            {...props}
-          />
-        </div>
-      );
-    }
-
-    return (
-      <input
-        ref={ref}
-        className={cn(inputClass, className)}
-        {...props}
-      />
+        )}
+        <input
+          ref={ref}
+          className={cn(
+            error ? 'input-error' : 'input',
+            icon && 'pl-10',
+            className
+          )}
+          {...props}
+        />
+      </div>
     );
   }
 );
